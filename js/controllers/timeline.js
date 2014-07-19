4c7115ccd0abacd647fbb0772d5cf724381ca69f
mcgowan <mike.mcgowan@gmail.com>
2014-07-18 18:09:52 -0700

    timeline work in progress.

--- js/controllers/timeline.js
+++ js/controllers/timeline.js
@@ -1,11 +1,28 @@
 App.controller('TimelineCtl', function TimelineCtl($scope) {
+    var getDateRangeText = function(days) {
+    	return moment().subtract('days', days).format('MMMM D, YYYY').toUpperCase() + ' - ' + moment().format('MMMM D, YYYY').toUpperCase();
+    };
+
     $scope.init = function() {
 		$scope.timeline = new Timeline('customer_timeline');
 		$scope.timeline.draw();
+
+		var days = $scope.timeline.getDays();
+		console.log('days ' + days);
+
+		// $scope.dateRange = getDateRangeText($scope.timeline.getDays());
+		$scope.dateRange = getDateRangeText(days);
+
     };
 
 	$scope.resize = function(event) {
 		$scope.timeline.redraw();
+
+		var days = $scope.timeline.getDays();
+		console.log('days ' + days);
+
+		// $scope.dateRange = getDateRangeText($scope.timeline.getDays());
+		$scope.dateRange = getDateRangeText(days);
 	};
 
     $scope.init();
--- js/timeline.js
+++ js/timeline.js
@@ -1,5 +1,3 @@
-var debug = {};
-
 var Timeline = function(canvas) {
     this.canvas = document.getElementById(canvas);
 }
@@ -7,6 +5,130 @@ var Timeline = function(canvas) {
 Timeline.prototype = function() {
 
     var timeline = {};
+
+    var items = [ 
+        // { type: 'date' },
+        { type: 'head', text: 'Hardware Products', showDates: true },
+        { type: 'item', text: 'Decoder', history: [ 
+            { day: -180, status: 1 },
+            { day: -5, status: 2 },
+        ] },
+        { type: 'item', text: 'HD Decoder', history: [ 
+            { day: -8, status: 1 },
+            { day: -5, status: 3 },
+        ] },
+        { type: 'item', text: 'Smartcard', history: [ 
+            { day: -180, status: 1 },
+            { day: -5, status: 7 },
+            { day: -4, status: 6 },
+            { day: -3, status: 1 },
+        ] },
+        // { type: 'none', text: '' },
+        { type: 'head', text: 'Software Products', showDates: false },
+        { type: 'item', text: 'TV Basic', history: [ 
+            { day: -180, status: 1 },
+        ] },
+        // { type: 'item', text: 'TV Gold Package', history: [ 
+        //     { day: -180, status: 1 },
+        //     { day: -10, status: 2 },
+        //     { day: -9, status: 5 },
+        //     { day: -3, status: 0 },
+        // ] },
+        // { type: 'item', text: 'TV Silver Package', history: [ 
+        //     { day: -10, status: 4 },
+        //     { day: -9, status: 1 },
+        // ] },
+        { type: 'item', text: 'VoIP Basic', history: [ 
+            { day: -10, status: 1 },
+            { day: -7, status: 2 },
+            { day: -6, status: 0 },
+        ] },
+        { type: 'none', text: '' },
+        { type: 'head', text: 'Contacts', showDates: false },
+        { type: 'item', text: 'Phone' },
+        { type: 'item', text: 'Email' },
+        { type: 'item', text: 'Other' },
+    ];
+
+    var months = [
+        { id:  0, text: 'JAN', color: '#4C87B9' },
+        { id:  1, text: 'FEB', color: '#E14E4E' },
+        { id:  2, text: 'MAR', color: '#3FABA4' },
+        { id:  3, text: 'APR', color: '#7F6CA1' },
+        { id:  4, text: 'MAY', color: '#4C87B9' },
+        { id:  5, text: 'JUN', color: '#E14E4E' },
+        { id:  6, text: 'JUL', color: '#3FABA4' },
+        { id:  7, text: 'AUG', color: '#7F6CA1' },
+        { id:  8, text: 'SEP', color: '#4C87B9' },
+        { id:  9, text: 'OCT', color: '#E14E4E' },
+        { id: 10, text: 'NOV', color: '#3FABA4' },
+        { id: 11, text: 'DEC', color: '#7F6CA1' },
+    ];
+
+    var defaults = {
+        margin: { top: 18, left: 5 },
+        days: 14,
+        lineHeight: 25,
+        lineColor: '#eeeeee',
+        // lineColor: '#CCCCCC',
+        
+        cellWidth: 25,
+        maxDetailWidth: 250,
+        itemHeaderFont: {
+            fontFamily: 'Open Sans',
+            fontColor: '#000000',
+            fontSize: '14px',
+            fontBold: true,
+        },
+        itemFont: {
+            fontFamily: 'Open Sans',
+            fontColor: '#428bca',
+            fontSize: '14px',
+            fontBold: false,
+        },
+        status: [ 
+            { id: 1, color: '#A6D96A', text: 'Active' },
+            { id: 2, color: '#F46D43', text: 'Cancelled' },
+            { id: 3, color: '#D73027', text: 'Non-pay Disconnect' },
+            { id: 4, color: '#FFFFBF', text: 'Waiting Activation' },
+            { id: 5, color: '#FEE08B', text: 'Pending Removal' },
+            { id: 6, color: '#D9EF8B', text: 'Scheduled Activation' },
+            { id: 7, color: '#FDAE61', text: 'Suspended' },
+        ],
+    };
+
+    // timeline.getMonthAbbr = function(month) {
+    //     var months = [
+    //         { id:  0, text: 'JAN' },
+    //         { id:  1, text: 'FEB' },
+    //         { id:  2, text: 'MAR' },
+    //         { id:  3, text: 'APR' },
+    //         { id:  4, text: 'MAY' },
+    //         { id:  5, text: 'JUN' },
+    //         { id:  6, text: 'JUL' },
+    //         { id:  7, text: 'AUG' },
+    //         { id:  8, text: 'SEP' },
+    //         { id:  9, text: 'OCT' },
+    //         { id: 10, text: 'NOV' },
+    //         { id: 11, text: 'DEC' },
+    //     ];
+    //     return months[month].text;
+    // };
+
+    // timeline.getRandomColor = function() {
+    //     var colors = [
+    //         { id: 1, color: '#4C87B9' },
+    //         { id: 2, color: '#E14E4E' },
+    //         { id: 3, color: '#3FABA4' },
+    //         { id: 4, color: '#7F6CA1' },
+    //     ];
+
+    //     var i = Math.floor((Math.random() * colors.length) + 1);
+
+    //     console.log(i);
+
+    //     return colors[i - 1].color;
+    // };
 
     timeline.drawLine = function(x1, y1, x2, y2, color, width) {
     	return timeline.paper.path('M' + x1 + ' ' + y1 + 'L' + x2 + ' ' + y2).attr({
@@ -52,12 +174,17 @@ Timeline.prototype = function() {
 
     };
 
+                    // timeline.drawLineDate(defaults, i * defaults.lineHeight)
+    timeline.drawLineDate = function(defaults, start) {
+
+    };
+
     timeline.drawLineHeader = function(defaults, text, start, days) {
         var background = timeline.drawRect(0, start + 2, defaults.canvas.width, defaults.lineHeight - 4, 0, '#FAFAFA', 1, '#FAFAFA');
 
 
         var y = start + (defaults.lineHeight/2) + 1;
-        timeline.drawText(text, defaults.leftMargin, y, defaults.itemHeaderFont);
+        timeline.drawText(text, defaults.margin.left, y, defaults.itemHeaderFont);
 
         y = start + defaults.lineHeight - 2;
 
@@ -73,84 +200,69 @@ Timeline.prototype = function() {
         timeline.drawRect(0, y, defaults.canvas.width, 1, 0, color, 1, color);
 
         
-
         if (days) {
-            // background.remove();
-
             var today = new Date();
 
-            // var x = defaults.canvas.width - (defaults.days * defaults.cellWidth);
             var x = defaults.canvas.width;
 
             for (var i = defaults.days - 1; i > 0; i--) {
                 var d = today.getDate();
+                var m = today.getMonth();
 
                 x -= defaults.cellWidth;
 
-                // if (d === 1) {
-                //     timeline.paper.text(x + (defaults.cellWidth/2), start + 5, 'MAR').attr({
-                //     'font-family': 'RobotoL',
-                //     'font-size': '8px',
-                //     // 'font-weight': 'bold',
-                //         'fill': '#A6A6A6'
-                //     });
+                if ((d === 1) || (i === 1)) {
 
-                // }
+                    var dx = x, dy = y - defaults.lineHeight - 12, dw = defaults.cellWidth, dh = 13;
 
-                // timeline.paper.text(x + (defaults.cellWidth/2), (y/2) + 5, d).attr({
-                //     'font-family': 'RobotoL',
+                    timeline.drawRect(dx, dy, dw, dh, 0, months[m].color, 1, months[m].color);
 
-                //         'font-size': '10px',
+                    timeline.paper.text(dx + (dw/2), (dy + dh)/2 + 2, months[m].text).attr({
+                        'font-family': 'RobotoL',
+                        'font-size': '10.5px',
+                        'fill': '#ffffff'
+                    });
 
-                //     'fill': '#414141'
-                // });
+                    var pw = 8, ph = 5;
 
-                console.log(x + ' ' + y);
+                    var px1 = (dx + (dw/2)) - (pw/2), py1 = dy + dh;
+                    var px2 = px1 + pw, py2 = py1;
+                    var px3 = px2 - (pw/2), py3 = py1 + ph;
 
-                // if (i % 2 === 0) {
-                //     // var c = '#87C3F6';
-                //     var c = '#88AED3';
+                    timeline.paper.path('M' + px1 + ',' + py2 + ',' + px2 + ',' + py2 + ',' + px3 + ',' + py3 + 'z').attr({
+                        'fill': months[m].color, 'stroke': months[m].color
+                    });
+                }
 
-                //     // var c = '#EDF3F9';
+                if (i === defaults.days - 1) {
+                    var c = '#D73027';
 
-                //     // EDF3F9
+                    timeline.drawRect(x + 3, y - defaults.lineHeight + 5, defaults.cellWidth - 6, defaults.lineHeight - 7, 0, c, 1, c);
 
-                //     timeline.drawRect(x + 3, 3, defaults.cellWidth - 6, defaults.lineHeight - 8, 0, c, 1, c);
-                //         }
-
-
-                if (d === 1) {
-                    var c = '#FFAF59';
-                    // var c = '#EDF3F9';
-
-                    timeline.drawRect(x + 3, 3, defaults.cellWidth - 6, defaults.lineHeight - 7, 0, c, 1, c);
-                        }
-
-                timeline.paper.text(x + (defaults.cellWidth/2), y - defaults.lineHeight/2 + 2, d).attr({
-                    // 'font-family': 'RobotoL',
-
-                    'font-family': 'Open Sans',
-
-                        'font-size': '11.5px',
-
-                    // 'fill': '#414141'
-                    // 'fill': '#80898e'
-                    'fill': '#000000'
-                });
+                    timeline.paper.text(x + (defaults.cellWidth/2), y - defaults.lineHeight/2 + 2, d).attr({
+                        'font-family': 'Open Sans',
+                        'font-size': '11px',
+                        'fill': '#ffffff'
+                    });
+                }
+                else {
+                    timeline.paper.text(x + (defaults.cellWidth/2), y - defaults.lineHeight/2 + 2, d).attr({
+                        'font-family': 'Open Sans',
+                        'font-size': '11px',
+                        'fill': '#000000'
+                    });
+                }
 
                 today.setDate(today.getDate() - 1);
-
-
-
-
             };
-
         }
     };
 
     timeline.drawLineItem = function(defaults, item, start) {
         var y = start + (defaults.lineHeight/2);
-        timeline.drawText(item.text, defaults.leftMargin, y, defaults.itemFont);
+        timeline.drawText(item.text, defaults.margin.left, y, defaults.itemFont).attr({
+            'cursor': 'pointer'
+        });
 
         y = start + defaults.lineHeight;
         timeline.drawLine(0, y, defaults.canvas.width, y, defaults.lineColor);
@@ -180,8 +292,11 @@ Timeline.prototype = function() {
                         color = _.filter(defaults.status, function(s){
                             return s.id === history.status;
                         })[0].color;
+                        
+                        timeline.drawRect(x + 3, start + 3, defaults.cellWidth - 6, defaults.lineHeight - 6, 0, color, 1, color).attr({
+                            'cursor': 'pointer'
+                        });
 
-                        timeline.drawRect(x + 3, start + 3, defaults.cellWidth - 6, defaults.lineHeight - 6, 0, color, 1, color);
                     }
                 }
             }
@@ -193,22 +308,12 @@ Timeline.prototype = function() {
 
         for (var i = 0; i < defaults.days - 1; i++) {
             x += defaults.cellWidth;
-
-
+            
             var color = '#FFFFFF';
 
-            // var color = '#EDF3F9';
-
-
-
             if (i % 2 === 0) {
-                timeline.drawRect(x, 0, defaults.cellWidth, defaults.canvas.height, 0, color, 1, color);
-
+                timeline.drawRect(x, defaults.margin.top, defaults.cellWidth, defaults.canvas.height, defaults.margin.top, color, 1, color);
             }
-
-
-
-
         }
     };
 
@@ -223,93 +328,10 @@ Timeline.prototype = function() {
         // 6   Scheduled Activation
         // 7   Suspended
 
-        var items = [ 
-            { type: 'head', text: 'Hardware Products' },
-            { type: 'item', text: 'Decoder', history: [ 
-                { day: -180, status: 1 },
-                { day: -5, status: 2 },
-            ] },
-            { type: 'item', text: 'HD Decoder', history: [ 
-                { day: -8, status: 1 },
-                { day: -5, status: 3 },
-            ] },
-            { type: 'item', text: 'Smartcard', history: [ 
-                { day: -180, status: 1 },
-                { day: -5, status: 7 },
-                { day: -4, status: 6 },
-                { day: -3, status: 1 },
-            ] },
-            { type: 'none', text: '' },
-            { type: 'head', text: 'Software Products' },
-            { type: 'item', text: 'TV Basic', history: [ 
-                { day: -180, status: 1 },
-            ] },
-            { type: 'item', text: 'TV Gold Package', history: [ 
-                { day: -180, status: 1 },
-                { day: -10, status: 2 },
-                { day: -9, status: 5 },
-                { day: -3, status: 0 },
-            ] },
-            { type: 'item', text: 'TV Silver Package', history: [ 
-                { day: -10, status: 4 },
-                { day: -9, status: 1 },
-            ] },
-            { type: 'item', text: 'VoIP Basic', history: [ 
-                { day: -10, status: 1 },
-                { day: -7, status: 2 },
-                { day: -6, status: 0 },
-            ] },
-            { type: 'none', text: '' },
-            { type: 'head', text: 'Contacts' },
-            { type: 'item', text: 'Phone' },
-            { type: 'item', text: 'Email' },
-            { type: 'item', text: 'Other' },
-        ];
-
-        var defaults = {
-            leftMargin: 5, 
-            days: 14,
-            lineHeight: 25,
-            lineColor: '#eeeeee',
-            // lineColor: '#CCCCCC',
-            
-            cellWidth: 25,
-            maxDetailWidth: 250,
-            itemHeaderFont: {
-                fontFamily: 'Open Sans',
-                fontColor: '#000000',
-                fontSize: '14px',
-                fontBold: true,
-            },
-            itemFont: {
-                fontFamily: 'Open Sans',
-                fontColor: '#428bca',
-                fontSize: '14px',
-                fontBold: false,
-            },
-
-            // 1   Active
-            // 2   Cancelled
-            // 3   Non-pay Disc
-            // 4   Waiting Activation
-            // 5   Pending Removal
-            // 6   Scheduled Activation
-            // 7   Suspended
-
-            status: [ 
-                { id: 1, color: '#A6D96A' }, // Active
-                { id: 2, color: '#F46D43' }, // Cancelled
-                { id: 3, color: '#D73027' }, // Non-pay Disconnect
-                { id: 4, color: '#FFFFBF' }, // Waiting Activation
-                { id: 5, color: '#FEE08B' }, // Pending Removal
-                { id: 6, color: '#D9EF8B' }, // Scheduled Activation
-                { id: 7, color: '#FDAE61' }, // Suspended
-            ],
-        };
 
         defaults.canvas = {
             width: canvas.clientWidth,
-            height: items.length * defaults.lineHeight
+            height: (items.length * defaults.lineHeight) + defaults.margin.top
         }
 
         var l = defaults.canvas.width/3; // left
@@ -331,148 +353,18 @@ Timeline.prototype = function() {
 
         // debug.timeline = timeline;
         for (var i = 0; i < items.length; i++) {
+            
             if (items[i].type === 'head') {
-                timeline.drawLineHeader(defaults, items[i].text, i * defaults.lineHeight, i === 0)                    
+                timeline.drawLineHeader(defaults, items[i].text, (i * defaults.lineHeight) + defaults.margin.top, items[i].showDates)
             } else if (items[i].type === 'item') {
-                timeline.drawLineItem(defaults, items[i], i * defaults.lineHeight, i === 0)                    
+                timeline.drawLineItem(defaults, items[i], (i * defaults.lineHeight) + defaults.margin.top)
             } else {
                 // timeline.drawLineHeader(defaults, '', i * defaults.lineHeight, i === 0)                    
             }
         }
 
 
-
-        // var lineColor = '#eeeeee';
-
-        // var x, y, lineCount = 2;
-        // var lineHeight = 25;
-        // var days = 14;
-
-
-        // var width = canvas.clientWidth - 8;
-        // var height = lineHeight * lineCount;
-
-        // var top = lineHeight;
-        // // var top = 0;
-
-
-        // var bottom = top + height;
-        // var left = 0;
-        // var right = width;
-
-        // var cellMaxWidth = 25;
-
-        // timeline.drawRect(left, top, width, height, 0, lineColor);
-
-        // // draw horiz. lines
-        // y = top;
-        // for (var i = 0; i < lineCount - 1; i++) {
-        //     y += lineHeight;
-        //     timeline.drawLine(left, y, right, y, lineColor);
-        // }
-
-        // // draw cells
-        // var maxWidth = days * cellMaxWidth
-        // var cellsStart = right / 3;
-        // var minStart = right - maxWidth;
-
-        // x = cellsStart;
-        // if (cellsStart < minStart) { //use minStart
-        //     x = minStart;
-        // }
-
-
-        // // draw dates
-
-        // var day = (right - x) / days;
-
-        // for (var i = 0; i < days; i++) {
-        //     timeline.paper.text(x + (day/2), lineHeight/2, i).attr({
-        //         'font-family': 'RobotoL',
-        //         'font-size': '10px',
-        //         'fill': '#A6A6A6'
-        //     });
-        //     timeline.drawLine(x, top, x, bottom, lineColor);
-        //     x += day;
-        // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // timeline.drawLine(x * 2, 0, x * 2, lineHeight * lines, '#EEEEEE');
-
-
-
-
-        // timeline.drawLine(0, 50, 0, 150);
-        // timeline.drawLine(width, 50, width, 150);
-
-
-        // var f = {
-        //     fontFamily: 'Open Sans',
-        //     fontColor: '#000000',
-        //     fontSize: '14px',
-        //     fontBold: true,
-        // };
-
-        // x = left + 5
-        // y = 22;
-        // timeline.drawText('Hardware Products', x, y, f);
-
-        // f.fontBold = false;
-        // f.fontColor = '#428bca';
-
-        // y += lineHeight;
-        // timeline.drawText('HD Decoder', x, y, f);
-
-        // y += lineHeight;
-        // timeline.drawText('Smartcard', x, y, f);
-
-        // f.fontBold = true;
-        // f.fontColor = '#000000';
-
-        // y += lineHeight + 10;
-        // timeline.drawText('Software Products', x, y, f);
-
-        // f.fontBold = false;
-        // f.fontColor = '#428bca';
-
-        // y += lineHeight;
-        // timeline.drawText('Basic', x, y, f);
-
-        // y += lineHeight;
-        // timeline.drawText('Gold Package', x, y, f);
-
-        // f.fontBold = true;
-        // f.fontColor = '#000000';
-
-        // y += lineHeight + 10;
-        // timeline.drawText('Contacts', x, y, f);
-
-        // f.fontBold = false;
-        // f.fontColor = '#428bca';
-
-        // y += lineHeight;
-        // timeline.drawText('Phone', x, y, f);
-
-        // y += lineHeight;
-        // timeline.drawText('Email', x, y, f);
-
-        // y += lineHeight;
-        // timeline.drawText('Other', x, y, f);
-
     };
-
-    // #44B6AE
 
     timeline.destroy = function() {
         if (timeline.paper) {
@@ -482,18 +374,31 @@ Timeline.prototype = function() {
     };
 
     var draw = function() {
-        console.log('draw');
+        // console.log('draw');
     	timeline.draw(this.canvas);
     };
 
     var redraw = function() {
-        console.log('redraw');
+        // console.log('redraw');
         timeline.draw(this.canvas);
+    };
+
+    // var getDateRange = function() {
+    //     var today = new Date();
+    //     return { 
+    //         from: today.setDate(today.getDate() - defaults.days),
+    //         to: today,
+    //     };
+    // };
+
+    var getDays = function() {
+        return defaults.days - 2;
     };
 
     return {
         draw: draw,
-        redraw: redraw
+        redraw: redraw,
+        getDays: getDays,
     }
 
 }();
